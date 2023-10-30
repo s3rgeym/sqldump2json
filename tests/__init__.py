6f25f3a16c4c7cb06d890d862abf20491ca5c03e
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2023-10-30 23:03:15 +0300

    небольшие исправления + тесты

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,16 @@
 import unittest
+from pathlib import Path
 
 from sqldump2json import DumpParser
+
+DUMP_FILE = Path(__file__).parent / "dump.sql"
 
 
 class Test(unittest.TestCase):
     def setUp(self) -> None:
         self.parser = DumpParser()
 
-    def test(self) -> None:
+    def test_corrupted_data(self) -> None:
         # Тут две запятые, после того как встречен какой-то неожиданный токен,
         # мы игнорируем все токены до следующего INSERT
         sql = """
@@ -23,3 +26,14 @@ INSERT INTO users VALUES(DEFAULT, "Semyon", 52 / (4 - 2))
                 {"table_name": "users", "values": [None, "Semyon", 26]},
             ],
         )
+
+    def test_hex_string2bytes(self) -> None:
+        with DUMP_FILE.open() as fp:
+            for res in self.parser.parse(fp):
+                if res["table_name"] != "staff":
+                    continue
+                first_name, last_name, _, img_data = res["values"][1:5]
+                # Там фотка этого васяна
+                if first_name == "Mike" and last_name == "Hillyer":
+                    return self.assertEqual(b"\x89PNG", img_data[:4])
+        self.fail()
