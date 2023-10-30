f51b3e1d25ca58e27507cc05a278c500c1724581
None <yamldeveloper@proton.me>
2023-10-30 07:46:42 +0300

    переименовал классы, добавил цветной логгер, теперь метод parse принимает аргумент, токенайзер поддерживает строки и пр

---
+++ tests/__init__.py
@@ -0,0 +1,25 @@
+import unittest
+
+from sqldump2json import DumpParser
+
+
+class Test(unittest.TestCase):
+    def setUp(self) -> None:
+        self.parser = DumpParser()
+
+    def test_inserts(self) -> None:
+        sql = """
+INSERT/*lalalala*/INTO users(name,age)VALUES ('Petya', 31),
+('Vasay', 42), ('Semyon', 52 / (4 - 2))
+        """
+        self.assertEqual(
+            [*self.parser.parse(sql)],
+            [
+                {"table_name": "users", "values": {"age": 31, "name": "Petya"}},
+                {"table_name": "users", "values": {"age": 42, "name": "Vasay"}},
+                {
+                    "table_name": "users",
+                    "values": {"age": 26, "name": "Semyon"},
+                },
+            ],
+        )
